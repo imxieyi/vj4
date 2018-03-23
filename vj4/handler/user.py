40dd5327b69a2539fa6bdb52c3aae470728bbc5b
Xie Yi <wez733@live.cn>
2018-03-23 23:50:32 +0800

    hide pretest & show 50 items in profile page

--- vj4/handler/user.py
+++ vj4/handler/user.py
@@ -189,8 +189,8 @@ class UserDetailHandler(base.Handler, UserSettingsMixin):
                                        token.get_most_recent_session_by_uid(udoc['_id']))
 
     rdocs = record.get_multi(get_hidden=True,
-                             uid=uid).sort([('_id', -1)])
-    rdocs = await rdocs.limit(10).to_list()
+                             uid=uid, data_id=None).sort([('_id', -1)])
+    rdocs = await rdocs.limit(50).to_list()
     pdict = await problem.get_dict_multi_domain((rdoc['domain_id'], rdoc['pid']) for rdoc in rdocs)
 
     # check hidden problem
