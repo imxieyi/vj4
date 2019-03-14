c62c95f152ea47ac2b875b3cc93d52942e3d85d4
hguandl <hguandl@gmail.com>
2019-03-15 02:09:45 +0800

    add new languages

--- vj4/ui/constant/language.js
+++ vj4/ui/constant/language.js
@@ -6,6 +6,8 @@ export const LANG_TEXTS = {
   java: 'Java',
   py: 'Python',
   py3: 'Python 3',
+  go: 'Go',
+  js: "Node.js"
 };
 
 export const LANG_HIGHLIGHT_ID = {
@@ -14,6 +16,8 @@ export const LANG_HIGHLIGHT_ID = {
   java: 'java',
   py: 'python',
   py3: 'python',
+  go: 'go',
+  js: 'js'
 };
 
 export const LANG_CODEMIRROR_MODES = {
@@ -22,5 +26,7 @@ export const LANG_CODEMIRROR_MODES = {
   java: 'text/x-java',
   py: 'text/x-python',
   py3: 'text/x-python',
+  go: 'text/x-gosrc',
+  js: 'text/javascript'
 };
 attachObjectMeta(LANG_CODEMIRROR_MODES, 'exportToPython', false);
