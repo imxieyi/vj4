026d41ff152b199bf683736bb2954eaefdbb9d3e
hguandl <hguandl@gmail.com>
2019-04-27 15:37:10 +0800

    Support C11 and C++17

--- vj4/ui/constant/language.js
+++ vj4/ui/constant/language.js
@@ -2,7 +2,9 @@ import attachObjectMeta from './util/objectMeta';
 
 export const LANG_TEXTS = {
   c: 'C',
+  c11: 'C11',
   cc: 'C++',
+  cc17: 'C++17',
   java: 'Java',
   py: 'Python',
   py3: 'Python 3',
@@ -12,7 +14,9 @@ export const LANG_TEXTS = {
 
 export const LANG_HIGHLIGHT_ID = {
   c: 'c',
+  c11: 'c',
   cc: 'cpp',
+  cc17: 'cpp',
   java: 'java',
   py: 'python',
   py3: 'python',
@@ -22,7 +26,9 @@ export const LANG_HIGHLIGHT_ID = {
 
 export const LANG_CODEMIRROR_MODES = {
   c: 'text/x-csrc',
+  c11: 'text/x-csrc',
   cc: 'text/x-c++src',
+  cc17: 'text/x-c++src',
   java: 'text/x-java',
   py: 'text/x-python',
   py3: 'text/x-python',
